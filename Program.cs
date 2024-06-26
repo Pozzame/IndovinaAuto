32ab8adb2ab86bb9bbe3ead8803184de90029f5f
Pozzame <pozzame@gmail.com>
2024-06-26 14:23:18 +0200

    Prova binaria

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ int media=0;
 int ite = iterazioni;
 while (Convert.ToBoolean(iterazioni))
 {
-    media += TrovatoreRand(rng.Next(1000));
+    int segreto = rng.Next(100);
+    Console.WriteLine("Num da trovare " + segreto);
+    media += TrovatoreBinario(segreto);
     iterazioni--;
 }
 Console.WriteLine($"Ci ha messo mediamente {media/ite} volte.");
@@ -22,31 +24,30 @@ int TrovatoreRand(int num)
     return prove;
 }
 
-
-
-
-int TrovatoreBinario(int num, int start, int end)
+int TrovatoreBinario(int num)
 {
     int prove = 1;
+    int prova = 50;
+    while (prova != num)
+    {
+        Console.WriteLine("Sto provando " + prova);
+        Thread.Sleep(100);
+    if (num < prova)
+        prova = prova/2;
+    else if (num > prova)
+        prova = 100-prova + (prova/2);
+    prove++;
+    }
+    return prove;
+}
+/*
     int prova = ((end - start) /2) + start;
     if (num == prova)
-        return prova;
+        return prove;
     else if (num < prova)
         TrovatoreBinario(num, start, prova);
     else if (num>prova)
         TrovatoreBinario(num, prova, end);
-/*    while(num != Binario())
-    {
-        if (prova < num)
-        {
-
-        }
-        prove++;
-    }*/
     return prove;
 }
-
-int Binario(int inizio, int fine)
-{
-    return ((fine - inizio) /2) + inizio;
-}
+*/
